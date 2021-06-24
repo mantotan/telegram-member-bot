e8c74fb604808129be12766812640bbb06c7b553
Manto Tan <manto.tan@gmail.com>
2021-06-24 23:07:53 +0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='telegram-member-bot',
+    version='1.0.0',
+    packages=['telethon', 'SQLAlchemy', 'PyMySQL', 'python-dotenv', 'pprint'],
+    url='https://github.com/mantotan/telegram-member-bot',
+    license='MIT',
+    author='mantotan',
+    author_email='hermantotan@hatolabs.com',
+    description='Telegram Member Bot'
+)
