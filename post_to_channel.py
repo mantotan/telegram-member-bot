8bce3b4fb965531a50a6ae5864ba750d69c6b5b4
Manto Tan <manto.tan@gmail.com>
2021-06-25 06:12:34 +0700

    add post to channel

---
+++ post_to_channel.py
@@ -0,0 +1,57 @@
+import os
+from dotenv import load_dotenv
+from telethon import TelegramClient, connection
+import logging
+from telethon import sync, TelegramClient, events
+from telethon.tl.functions.messages import GetDialogsRequest, GetHistoryRequest
+import json
+from datetime import datetime, timedelta
+import pprint
+import pymysql
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.dialects.mysql import insert
+from sqlalchemy.orm import Session
+from telethon.tl.types import MessageEntityTextUrl
+from tables.channel_posts import ChannelPost
+
+logging.basicConfig(level=logging.WARNING)
+load_dotenv()
+
+engine = create_engine('mysql+pymysql://'+os.getenv('DB_USERNAME')+':'+os.getenv('DB_PASS')+'@'+os.getenv('DB_URL')+':'+os.getenv('DB_PORT')+'/'+os.getenv('DB_SCHEMA')+'')
+db_conn = engine.connect()
+session = Session(engine, future=True)
+
+def post_group():
+    data = session.query(ChannelPost).where(ChannelPost.is_posted == False).order_by(ChannelPost.posted_date).first()
+    if data is not None:
+        if data.url is not None and data.url != '':
+            message = data.message.replace('link', '<a href="' + data.url + '">link</a>')
+            print(message)
+            send_message(os.getenv('TL_READ_API_PHONE'), os.getenv('TL_READ_API_ID'), os.getenv('TL_READ_API_HASH'), message)
+            data.is_posted = True
+            session.commit()
+        else:
+            message = data.message
+            print(message)
+            send_message(os.getenv('TL_READ_API_PHONE'), os.getenv('TL_READ_API_ID'), os.getenv('TL_READ_API_HASH'), message)
+            data.is_posted = True
+            session.commit()
+
+def send_message(phone, api_id, api_hash, message):
+    folder_session = 'session/'
+    client = TelegramClient(folder_session + phone, api_id, api_hash)
+    client.connect()
+    if not client.is_user_authorized():
+        print('Login fail, need to run init_session')
+    else:
+        destination_channel_username = os.getenv('POST_TO_USERNAME')
+        entity = client.get_entity(destination_channel_username)
+        client.send_message(
+            entity=entity,
+            message=message,
+            link_preview=True,
+            parse_mode='html'
+        )
+
+post_group()
