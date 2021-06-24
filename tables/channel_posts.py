23080f9463579aae466a04f15ec5d135631cbc2e
Manto Tan <manto.tan@gmail.com>
2021-06-24 23:08:39 +0700

    Channel Posts table

---
+++ tables/channel_posts.py
@@ -0,0 +1,14 @@
+from sqlalchemy.orm import declarative_base
+Base = declarative_base()
+from sqlalchemy import Column, Integer, String, DateTime, Boolean
+
+class ChannelPost(Base):
+    __tablename__ = 'channel_posts'
+    id = Column(Integer, primary_key=True)
+    username = Column(String)
+    group_type = Column(String)
+    message_type = Column(String)
+    posted_date = Column(DateTime)
+    message_id = Column(String)
+    message = Column(Integer)
+    is_posted = Column(Boolean)
