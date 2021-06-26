80dd5dfed9d9b261a2a00af120ec3353a0fdb76d
Manto Tan <manto.tan@gmail.com>
2021-06-26 08:45:55 +0700

    init session using data from mysql database

--- init_session.py
+++ init_session.py
@@ -1,21 +1,30 @@
+import os
+import urllib.parse
+from pathlib import Path
+from dotenv import load_dotenv
 from telethon import TelegramClient, connection
 import logging
 from telethon import sync, TelegramClient, events
-import json
+from tables.my_bots import MyBot
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 
-with open('config.json', 'r') as f:
-	config = json.loads(f.read())
+load_dotenv()
+
+engine = create_engine('mysql+pymysql://'+os.getenv('DB_USERNAME')+':'+urllib.parse.quote(os.getenv('DB_PASS'))+'@'+os.getenv('DB_URL')+':'+os.getenv('DB_PORT')+'/'+os.getenv('DB_SCHEMA')+'')
+db_conn = engine.connect()
+session = Session(engine, future=True)
 
 logging.basicConfig(level=logging.WARNING)
 
-accounts = config['accounts']
+my_bots = session.query(MyBot).all()
 
-folder_session = 'session/'
+folder_session = str(Path(__file__).parent.absolute()) + str('/session/')
 
-for account in accounts:
-	api_id = account['api_id']
-	api_hash = account['api_hash']
-	phone = account['phone']
+for account in my_bots:
+	api_id = account.api_id
+	api_hash = account.api_hash
+	phone = account.phone
 	print(phone)
 
 	client = TelegramClient(folder_session + phone, api_id, api_hash)
